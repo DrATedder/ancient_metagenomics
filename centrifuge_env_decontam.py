9511feb408089d0c6442563404c03fcc2dba92b1
Andrew Tedder <54320061+DrATedder@users.noreply.github.com>
2023-09-19 11:20:51 +0100

    Update centrifuge_env_decontam.py
    Tweaked the 'genus' and 'species' functions to account for cases where 'leaf' OTU designation doesn't follow the expected pattern.

--- centrifuge_env_decontam.py
+++ centrifuge_env_decontam.py
@@ -17,8 +17,11 @@ def read_genus_or_below_list(env_sample):
         otu = line.split("\t")[0]
         if tax_rank in target_taxa:
             if tax_rank == "leaf":
-                otu_mod = f"{otu.split()[0]} {otu.split()[1]}"
-                otu_list.add(otu_mod)
+                if len(otu.split()) > 1:
+                    otu_mod = f"{otu.split()[0]} {otu.split()[1]}"
+                    otu_list.add(otu_mod)
+                else:
+                    otu = line.split("\t")[0]
             else:
                 otu_list.add(otu)
     return otu_list
@@ -31,8 +34,11 @@ def read_species_list(env_sample):
         otu = line.split("\t")[0].strip()
         if tax_rank in target_taxa:
             if tax_rank == "leaf":
-                otu_mod = f"{otu.split()[0]} {otu.split()[1]}"
-                otu_list.add(otu_mod)
+                if len(otu.split()) > 1:
+                    otu_mod = f"{otu.split()[0]} {otu.split()[1]}"
+                    otu_list.add(otu_mod)
+                else:
+                    otu = line.split("\t")[0]
             else:
                 otu_list.add(otu)
     return otu_list
